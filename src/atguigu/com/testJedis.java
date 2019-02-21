be96409566e883ae33914044c63e7a78481aeb8f
yang <yyz233@163.com>
2019-02-21 14:46:12 +0800

    first update

---
+++ src/atguigu/com/testJedis.java
@@ -0,0 +1,24 @@
+package atguigu.com;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisSentinelPool;
+
+public class testJedis {
+	
+	@SuppressWarnings("source")
+	@Test
+	public void test() {
+		
+		Jedis jedis = new Jedis("192.168.218.218",6379);
+		
+		String pong = jedis.ping();
+		
+		System.out.println(pong);
+	}
+	
+}
